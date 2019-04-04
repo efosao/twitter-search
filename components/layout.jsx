14d86221c9b6c3caf2ac8e814187a304218ab924
Efosa Oyegun <efosao@gmail.com>
2019-04-04 10:19:49 -0700

    sort imports

--- components/layout.jsx
+++ components/layout.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
 import Head from 'next/head'
+import React from 'react'
 import { Container, Grid, Header, Icon } from 'semantic-ui-react'
 
 const Layout = ({ children }) => {
