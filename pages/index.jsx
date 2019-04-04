2db3b314b64303a6c3d237840d1ca1f79f44d7ab
Efosa Oyegun <efosao@gmail.com>
2019-04-04 12:04:35 -0700

    added search results to UI + connected events

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,13 @@
 /* global fetch */
+import TimeAgo from 'javascript-time-ago'
+import en from 'javascript-time-ago/locale/en'
+import { useRef, useState } from 'react'
+import { Image, Input, List } from 'semantic-ui-react'
 import Layout from '../components/layout'
-import { Button, Input } from 'semantic-ui-react'
+
+// setup date/time formatter.
+TimeAgo.addLocale(en)
+const timeAgo = new TimeAgo('en-US')
 
 const getUserTimeline = async username => {
   try {
@@ -13,20 +20,58 @@ const getUserTimeline = async username => {
 }
 
 const Page = () => {
+  const [ loading, setLoading ] = useState(false)
+  const [ tweets, setTweets ] = useState([])
+  const inputRef = useRef()
+
   const searchUserTimeline = async () => {
-    console.log(await getUserTimeline('efosa'))
+    const username = inputRef.current.value
+    setLoading(true)
+    const result = await getUserTimeline(username)
+    setLoading(false)
+    const { tweets = [] } = result
+    setTweets(tweets)
   }
+
+  const searchOnEnter = e => {
+    if (e.which === 13) searchUserTimeline()
+  }
+
+  const tweetsBlock = tweets.map(t => (
+    <List.Item>
+      <Image avatar src={t.user.profile_image_url_https} />
+      <List.Content>
+        {t.in_reply_to_screen_name &&
+          <List.Header as='a'>RE: {t.in_reply_to_screen_name}</List.Header>}
+        <List.Description>
+        Last seen watching{' '}
+          <a>
+            <b>{t.text}</b>
+          </a>{' '}
+          {timeAgo.format(new Date(t.created_at))}.
+        </List.Description>
+      </List.Content>
+    </List.Item>
+  ))
 
   return (
     <Layout>
       <>
         <div>
-          <Input icon='at' iconPosition='left' placeholder='Search users...' />
-          <Button onClick={searchUserTimeline}>Search</Button>
+          <Input
+            action={{ content: 'Search', onClick: searchUserTimeline }}
+            icon='at'
+            iconPosition='left'
+            input={() => <input onKeyUp={searchOnEnter} ref={inputRef} />}
+            loading={loading}
+            placeholder='Search users...'
+          />
         </div>
 
         <section>
-          <p>Results</p>
+          <List relaxed>
+            {tweetsBlock}
+          </List>
         </section>
       </>
     </Layout>
