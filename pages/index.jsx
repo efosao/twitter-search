ba58bc74c3942bd0108ba96ec7dd2b82b6b5a2bf
Efosa Oyegun <efosao@gmail.com>
2019-04-04 11:08:11 -0700

    added initial front-end search function

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,28 @@
+/* global fetch */
 import Layout from '../components/layout'
 import { Button, Input } from 'semantic-ui-react'
 
+const getUserTimeline = async username => {
+  try {
+    const result = await fetch(`/api/search/${username}`)
+    if (result.ok) return await result.json()
+    else return { error: `${result.statusText} (${result.status})` }
+  } catch (e) {
+    return { error: e.message }
+  }
+}
+
 const Page = () => {
+  const searchUserTimeline = async () => {
+    console.log(await getUserTimeline('efosa'))
+  }
+
   return (
     <Layout>
       <>
         <div>
           <Input icon='at' iconPosition='left' placeholder='Search users...' />
-          <Button>Search</Button>
+          <Button onClick={searchUserTimeline}>Search</Button>
         </div>
 
         <section>
