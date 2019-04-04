f77272e54388cf18f621e50786fc9184550e4c29
Efosa Oyegun <efosao@gmail.com>
2019-04-04 11:17:04 -0700

    fixed env vars bug in server (dotenv)

--- server/app.js
+++ server/app.js
@@ -1,8 +1,7 @@
+require('dotenv').config()
 const express = require('express')
 const next = require('next')
 const routes = require('./routes')
-
-require('dotenv').config()
 
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
